ecac1fc2ae9198d6bccd67c54aaa99f030a9a175
Vitor Gama Lemos <31148456+vitorglemos@users.noreply.github.com>
2020-02-01 14:30:25 -0300

    Update clean_ssh_machines.py

--- clean-machines/clean_ssh_machines.py
+++ clean-machines/clean_ssh_machines.py
@@ -1,21 +1,21 @@
 import os
 
-machines = ["22","32","34","37","39"];
+machines = ["22","32","34","37","39"]; ## array with all machines on cluster
 
 def machines_tmp_delete():
     for i in range(0,len(machines)):
-        ssh = "ssh vlemos@" + machines[i];
-        delete = " 'rm -r /tmp/* -f >/dev/null 2>&1'";
+        ssh = "ssh my_user_ssh@" + machines[i]; # your ssh user
+        delete = " 'rm -r /tmp/* -f >/dev/null 2>&1'"; # temp files
         command = ssh + delete;
         out = os.system(command);
         print(" Machine uff" + machines[i] + " /tmp deleted ! Success !")
         
 def machines_datanode_format():
     for i in range(0,len(machines)):
-        ssh = "ssh vlemos@uff" + machines[i];
-        directory = r" 'cd /var/usuarios/vitor/hadop'";
+        ssh = "ssh my_user_ssh@" + machines[i];
+        directory = r" 'cd /var/user'"; # your directory
         delete = " 'rm -r hdfs/'";
-        make = " 'mkdir -p hdfs/datanode'";
+        make = " 'mkdir -p hdfs/datanode'"; # clean datanodes
         chmod = " 'chmod 775 hdfs/datanode'";
         os.system(ssh + directory);
         os.system(ssh + delete);
@@ -24,5 +24,5 @@ def machines_datanode_format():
         print(" Machine uff" + machines[i] + " HDFS Datanode formated ! Success !")
       
 
-machines_tmp_delete();
-machines_datanode_format();
+machines_tmp_delete(); #this code clean /temp
+machines_datanode_format(); #this code format datanode directory 
